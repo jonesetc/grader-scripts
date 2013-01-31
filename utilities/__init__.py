d6d40ee62559285826c7daa6fa6da5297d1e104c
Kyle Jones <kyle@jonesetc.com>
2013-01-30 23:22:47 -0600

    added organize to utilities

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import shutil
 
 def getdirs():
     files = os.listdir('.')
@@ -13,3 +15,24 @@ def getfiles():
     files = [f for f in files if os.path.isfile(f)]
 
     return files
+
+def organize(subdir):
+    # Since each student has a directory, we must list all directories
+    students = getdirs()
+
+    for student in students:
+        # Go into each directory
+        os.chdir(student)
+
+        # Make the new directory in the student folder
+        os.mkdir(sys.argv[1])
+
+        # List all of the files in the student directory
+        files = getfiles()
+
+        # Move all of the files into the new directory
+        for file in files:
+            shutil.move(file, sys.argv[1])
+
+        # Get back into the beginning directory
+        os.chdir('..')
